c20d372cbbd7c69e8430ed551a03296c94ddf244
Caleb Smith <karagenit@outlook.com>
2018-03-06 00:04:16 -0500

    Add URL Checking

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 var http = require('http');
 var rest = require('./restapi');
+var url  = require('url');
 var fs   = require('fs');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-
     var token = fs.readFileSync('tba.token', 'utf8').trim();
 
     //handle url breakdown and set options
 
-    var options = {
-        host: 'www.thebluealliance.com',
-        port: 443,
-        path: '/api/v3/team/frc868',
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'User-Agent': 'frc868',
-            'X-TBA-Auth-Key': token
-        }
-    };
+    var path = url.parse(req.url).pathname;
 
-    rest.getJSON(options, function(statusCode, result) {
-        res.end(JSON.stringify(result));
-    });
+    if (path === '/events') {
+        var options = {
+            host: 'www.thebluealliance.com',
+            port: 443,
+            path: '/api/v3/team/frc868/events/2018/simple',
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'User-Agent': 'frc868',
+                'X-TBA-Auth-Key': token
+            }
+        };
+
+        rest.getJSON(options, function(statusCode, result) {
+            res.end(JSON.stringify(result));
+        });
+    }
 }).listen(8080);
