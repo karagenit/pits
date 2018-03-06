a9676597963cf5648b4ad80a49d311629e55deb0
Caleb Smith <karagenit@outlook.com>
2018-03-05 23:41:06 -0500

    Add Basic Github Request

--- index.js
+++ index.js
@@ -3,5 +3,21 @@ var rest = require('./restapi');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello!!');
+
+    //handle url breakdown and set options
+
+    var options = {
+        host: 'api.github.com',
+        port: 443,
+        path: '/users/karagenit',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'User-Agent': 'karagenit'
+        }
+    };
+
+    rest.getJSON(options, function(statusCode, result) {
+        res.end(JSON.stringify(result));
+    });
 }).listen(8080);
